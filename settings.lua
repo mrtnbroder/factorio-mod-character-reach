ab7317237346b4a479d9d41c2b39fbcf0bec10dc
Theis <theis99999@gmail.com>
2024-01-04 11:18:14 +0100

    Start

---
+++ settings.lua
@@ -0,0 +1,13 @@
+SETTING_NAME = "theis_character-reach-indicator_"
+
+--[[data:extend(
+{
+  {
+    type = "string-setting",
+    name = SETTING_NAME.."1",
+    setting_type = "runtime-global",
+    default_value = "",
+    allow_blank = true,
+    order = "q1",
+  },
+})]]
